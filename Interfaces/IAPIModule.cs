02968375d92ebad9acc9a06da8b6e518563373dd
Donovan Solms <donovan.solms@gmail.com>
2016-12-01 17:07:14 +0200

    Added API extending interfaces

---
+++ Interfaces/IAPIModule.cs
@@ -0,0 +1,33 @@
+﻿/** 
+* This file is part of Project Limitless.
+* Copyright © 2016 Donovan Solms.
+* Project Limitless
+* https://www.projectlimitless.io
+* 
+* Project Limitless is free software: you can redistribute it and/or modify
+* it under the terms of the Apache License Version 2.0.
+* 
+* You should have received a copy of the Apache License Version 2.0 with
+* Project Limitless. If not, see http://www.apache.org/licenses/LICENSE-2.0.
+*/
+
+using System;
+using System.Collections.Generic;
+
+using Limitless.Runtime.Types;
+
+namespace Limitless.Runtime
+{
+    /// <summary>
+    /// The interface required to be implemented by all modules
+    /// that adds API routes to the Project Limitless API.
+    /// </summary>
+    public interface IAPIModule
+    {
+        /// <summary>
+        /// Returns the list of available routes for the module.
+        /// </summary>
+        /// <returns>The list of routes</returns>
+        List<APIRouteHandler> GetRoutes();
+    }
+}
---
+++ Types/APIRouteHandler.cs
@@ -0,0 +1,36 @@
+﻿/** 
+* This file is part of Project Limitless.
+* Copyright © 2016 Donovan Solms.
+* Project Limitless
+* https://www.projectlimitless.io
+* 
+* Project Limitless is free software: you can redistribute it and/or modify
+* it under the terms of the Apache License Version 2.0.
+* 
+* You should have received a copy of the Apache License Version 2.0 with
+* Project Limitless. If not, see http://www.apache.org/licenses/LICENSE-2.0.
+*/
+
+using System;
+
+namespace Limitless.Runtime.Types
+{
+    /// <summary>
+    /// Defines a route handler for extending the Project Limitless API.
+    /// </summary>
+    public class APIRouteHandler
+    {
+        /// <summary>
+        /// The route. See documentation on route formats at 
+        /// https://docs.projectlimitless.io/ProjectLimitless/api-routes
+        /// </summary>
+        public string Route { get; set; } = "/";
+        /// <summary>
+        /// The handler function. Takes a dynamic object as input and
+        /// returns a dynamic object.
+        /// The method signature must be
+        /// public dynamic MyHandler(dynamic input)
+        /// </summary>
+        public Func<dynamic, dynamic> Handler { get; set; } = (dynamic input) => { return $"Missing Handler"; };
+    }
+}
