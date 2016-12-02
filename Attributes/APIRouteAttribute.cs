4e4eb49ff0d9ef85f45b9607b84e7842ac9d0e41
Donovan Solms <donovan.solms@gmail.com>
2016-12-02 23:35:48 +0200

    Adding custom attributes for extending the API

---
+++ Attributes/APIRouteAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Limitless.Runtime.Attributes
+{
+    /// <summary>
+    /// Defines a method as being an API Route.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class APIRouteAttribute : Attribute
+    {
+        /// <summary>
+        /// The route. See documentation on route formats at 
+        /// https://docs.projectlimitless.io/ProjectLimitless/api-routes
+        /// </summary>
+        public string Path { get; set; }
+    }
+}
