c830abd55781b818335cf36bdf47edd534ab8d16
Donovan Solms <donovan.solms@gmail.com>
2017-01-26 22:54:39 +0200

    Added headers and API requests objects

---
+++ Runtime/Types/APIRequest.cs
@@ -0,0 +1,54 @@
+﻿/** 
+* This file is part of Project Limitless.
+* Copyright © 2017 Donovan Solms.
+* Project Limitless
+* https://www.projectlimitless.io
+* 
+* Project Limitless is free software: you can redistribute it and/or modify
+* it under the terms of the Apache License Version 2.0.
+* 
+* You should have received a copy of the Apache License Version 2.0 with
+* Project Limitless. If not, see http://www.apache.org/licenses/LICENSE-2.0.
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Limitless.Runtime.Types
+{
+    /// <summary>
+    /// Defines a request to an API method.
+    /// </summary>
+    public class APIRequest
+    {
+        /// <summary>
+        /// The request headers as a Nancy Header holder.
+        /// </summary>
+        public NancyRequestHeaders Headers { get; set; }
+        /// <summary>
+        /// Url parameters extracted by Nancy.
+        /// </summary>
+        public dynamic Parameters { get; set; }
+        /// <summary>
+        /// The parsed querystring as Name, Value pairs
+        /// </summary>
+        public Dictionary<string, string> QueryString { get; set; }
+        /// <summary>
+        /// Data sent for POST and PUT requests.
+        /// </summary>
+        public dynamic Data { get; set; }
+        /// <summary>
+        /// The authenticated user.
+        /// </summary>
+        public BaseUser AuthenticatedUser { get; set; }
+
+        /// <summary>
+        /// Standard constructor.
+        /// </summary>
+        public APIRequest()
+        {
+            Headers = new NancyRequestHeaders();
+            QueryString = new Dictionary<string, string>();
+        }
+    }
+}
